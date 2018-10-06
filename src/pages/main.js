cf5be43647e509fcdee9637274c534a8d6cffab1
lucasrodrigues10 <lucasrodrigues10@outlook.com>
2018-10-06 12:08:30 -0300

    initial commit

---
+++ src/pages/main.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import {View, Text} from 'react-native';
+
+export default class Main extends Component {
+    static navigationOptions = {
+        title: 'Sustek'
+    };
+
+    render() {
+        return (
+            <View>
+                <Text> Pagina Legal </Text>
+            </View>
+        );
+    }
+}
